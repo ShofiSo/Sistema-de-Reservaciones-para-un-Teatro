301e940bae91f2e609d48bf140ddf5e6411d0f97
ShofiSo <kelly.rabanales@gmail.com>
2025-03-07 20:52:33 -0600

    Sexto Commit
    Cree los casos, llamando a las opciones del menú, me guie en base a mi código anterior...

--- Teatro.java
+++ Teatro.java
@@ -73,4 +73,41 @@ public class Teatro {
             System.out.println("4. Mostrar reservaciones");
             System.out.println("5. Salir");
             System.out.print("Seleccione una opción: ");
-        }}}
+            
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); 
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el nombre del cliente: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("Ingrese el número de asiento: ");
+                    int asiento = scanner.nextInt();
+                    gestor.registrarReservacion(nombre, asiento);
+                    break;
+                case 2:
+                    System.out.print("Ingrese el número de asiento a cancelar: ");
+                    int asientoCancelar = scanner.nextInt();
+                    gestor.cancelarReservacion(asientoCancelar);
+                    break;
+                case 3:
+                    System.out.print("Ingrese el número de asiento a verificar: ");
+                    int asientoVerificar = scanner.nextInt();
+                    if (gestor.estaOcupado(asientoVerificar)) {
+                        System.out.println("El asiento " + asientoVerificar + " está ocupado.");
+                    } else {
+                        System.out.println("El asiento " + asientoVerificar + " está disponible.");
+                    }
+                    break;
+                case 4:
+                    gestor.mostrarReservaciones();
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }
+    }
+}
