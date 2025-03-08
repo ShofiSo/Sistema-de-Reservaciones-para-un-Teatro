ce0ec1c4775542e70434b0f5791c16ae38c5f6ab
ShofiSo <kelly.rabanales@gmail.com>
2025-03-07 20:29:58 -0600

    Cuarto Commit
    Creando las clases que

--- Teatro.java
+++ Teatro.java
@@ -14,3 +14,38 @@ class Reservacion {
         return "Cliente: " + nombre + ", Asiento: " + numeroAsiento;
     }
 }
+class GestorBoletos {
+    private List<Reservacion> reservaciones;
+
+    public GestorBoletos() {
+        reservaciones = new ArrayList<>();
+    }
+
+    public void registrarReservacion(String nombre, int numeroAsiento) {
+        if (!estaOcupado(numeroAsiento)) {
+            reservaciones.add(new Reservacion(nombre, numeroAsiento));
+            System.out.println("Reservación registrada: " + nombre + " en asiento " + numeroAsiento);
+        } else {
+            System.out.println("El asiento " + numeroAsiento + " ya está ocupado.");
+        }
+    }
+    public void cancelarReservacion(int numeroAsiento) {
+        for (int i = 0; i < reservaciones.size(); i++) {
+            if (reservaciones.get(i).numeroAsiento == numeroAsiento) {
+                reservaciones.remove(i);
+                System.out.println("Reservación cancelada para el asiento " + numeroAsiento);
+                return;
+            }
+        }
+        System.out.println("No se encontró la reservación para el asiento " + numeroAsiento);
+    }
+
+    public boolean estaOcupado(int numeroAsiento) {
+        for (Reservacion r : reservaciones) {
+            if (r.numeroAsiento == numeroAsiento) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
